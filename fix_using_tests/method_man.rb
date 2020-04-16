242c5459a9a3fa6bfc69704aadb0922d13f32e51
Yannick Djoumbi <ydjoumbi@gmail.com>
2020-04-16 17:15:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
+
 def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
-  play_game()
+  play_game
 end
 
 def play_game(player1, player2)
   puts "#{player1} is better than #{player2}"
 end
+binding.pry
