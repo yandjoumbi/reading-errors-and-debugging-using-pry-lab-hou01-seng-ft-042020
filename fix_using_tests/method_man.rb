aa259d8ad739b6d608441b475749e636a21157ca
Yannick Djoumbi <ydjoumbi@gmail.com>
2020-04-16 17:08:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{playar2}"
+  puts "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
